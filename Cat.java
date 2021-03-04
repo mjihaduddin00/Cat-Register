273535e6ddbe6658fa9bdcb339b082af8feeec65
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-04 15:22:44 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Cat.java
@@ -0,0 +1,16 @@
+/* 
+Cat register
+
+*/
+
+public class Cat {
+    private static int catCount = 0;
+
+    public static void addNewCat() {
+        Cat.catCount = catCount + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
